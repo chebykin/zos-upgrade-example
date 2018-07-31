0a5207cb41f31aaac332287b367c6382cecbb811
Nikita Chebykin <nikita@chebyk.in>
2018-08-01 00:39:37 +0200

    Change truffle.js config

--- truffle.js
+++ truffle.js
@@ -1,18 +1,23 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+const Ganache = require('ganache-core');
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*'
+    },
+    test: {
+      provider: Ganache.provider({
+        unlocked_accounts: [0, 1, 2, 3, 4, 5]
+      }),
+      network_id: '*'
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
 };
