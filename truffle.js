acf77ef699acd0dc9879e1f8e47c77a96b5cb6de
Nikita Chebykin <nikita@chebyk.in>
2018-08-01 02:13:11 +0200

    Add parity configs

--- truffle.js
+++ truffle.js
@@ -7,6 +7,12 @@ module.exports = {
       port: 7545,
       network_id: '*'
     },
+    parity: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
+      gasLimit: "7900000"
+    },
     test: {
       provider: Ganache.provider({
         unlocked_accounts: [0, 1, 2, 3, 4, 5]
