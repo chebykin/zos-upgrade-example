989dc94028ec3072bc88ff4e35c4e461f4b3273a
Nikita Chebykin <nikita@chebyk.in>
2018-08-01 12:29:34 +0200

    Fix initializer call

--- test/testMyContract.js
+++ test/testMyContract.js
@@ -1,7 +1,7 @@
 const MyContract_v0 = artifacts.require('./MyContract_v0.sol');
 const AdminUpgradeabilityProxy = artifacts.require('zos-lib/contracts/upgradeability/AdminUpgradeabilityProxy.sol');
 
-contract('MyContract', () => {
+contract('MyContract', ([_, alice, bob]) => {
     describe('proxy', () => {
         it('should upgrade successfully', async function () {
             console.log('start');
@@ -9,7 +9,7 @@ contract('MyContract', () => {
             const proxy = await AdminUpgradeabilityProxy.new(myContract_v0.address);
             let myContract = await MyContract_v0.at(proxy.address);
             const value = 42;
-            await myContract.initialize(value);
+            await myContract.initialize(value, { from: alice });
         });
     });
 });
